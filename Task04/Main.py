2a3961d2602247e4b31258000fe9c323dd766420
Aleksandr <bestprogreraxe@gmail.com>
2023-03-24 21:54:36 +0300

    Solved task 4

--- Task04/Main.py
+++ Task04/Main.py
@@ -5,3 +5,11 @@
 3 2 4 -> yes
 3 2 1 -> no
 """
+
+length_of_choco = int(input('Enter length of chocolate: '))
+width_of_choco = int(input('Enter width of chocolate: '))
+size_of_piece = int(input('Enter size of chocolate squares on the broken piece: '))
+if size_of_piece % length_of_choco == 0 or size_of_piece % width_of_choco == 0:
+   print('Yeah, you can do it!')
+else:
+    print('Nope, you can\'t do it (if you are not a cheater).')
